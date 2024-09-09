5ceddb150e680338c90d702fb1f7aa1ec4c4ddfe
medayoubadri <medayoubadri@gmail.com>
2024-09-09 16:07:10 +0100

    Task 1: update fix

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(str * 3) # Print the string 3 times
-print(str[:9]) # Gets the first 9 characters
+print(str * 3)
+print(str[:9])
--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str1 = str1 + " " + str2 # Concatenate str1 & str2 with a space in between 
+str1 = str1 + " " + str2 
 print(f"Welcome to {str1}!")
