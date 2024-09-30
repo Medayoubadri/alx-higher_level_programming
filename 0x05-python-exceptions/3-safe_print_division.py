dd2f77fe1776f6efeae2ba60d5880ee4fb52db19
medayoubadri <medayoubadri@gmail.com>
2024-09-30 21:44:15 +0100

    Fix all tasks errors...

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,6 @@ def safe_print_division(a, b):
 	try:
 		result = a / b
 	except ZeroDivisionError:
-		# Caught trying to break the universe with a zero divisor. We say no.
 		result = None
 	finally:
 		print("Inside result: {}".format(result))
