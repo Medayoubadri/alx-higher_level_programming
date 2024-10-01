a658615c16e38799a1b4c530f8d8d7c06fc76e38
medayoubadri <medayoubadri@gmail.com>
2024-10-01 21:54:07 +0100

    First mandatory task..

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module that defines an empty class `Square`.
+"""
+
+
+class Square:
+    """
+    This is an empty class that defines a square.
+    Yep, it's emptier than my wallet at the end of the month.
+    """
+
+    pass
