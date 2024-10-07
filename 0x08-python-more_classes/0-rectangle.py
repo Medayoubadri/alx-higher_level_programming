20fc61401e06c5cfef0e8e97da08372df51249e5
medayoubadri <medayoubadri@gmail.com>
2024-10-07 12:17:52 +0100

    All work done Waiting for the checker

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines an empty Rectangle class.
+Why is it empty, you ask? It’s just the start...
+every masterpiece has a humble beginning.
+"""
+
+
+class Rectangle:
+    """An empty Rectangle class just chilling for now."""
+    pass
