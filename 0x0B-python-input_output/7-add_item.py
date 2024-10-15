ec64ada232e23d6ec8dbb7db0b6584a125fc05e2
medayoubadri <medayoubadri@gmail.com>
2024-10-15 18:57:06 +0100

    Task 6: Load a Python object from a JSON file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """
-Adds all command-line arguments to a Python list and saves them
-to a JSON file. It's like collecting things and hoarding them forever.
+A script that adds command-line arguments to a list and saves them to a file.
+Because who doesn't love keeping track of all their arguments in a JSON file?
 """
 
 import sys
+import os.path
 
+# Import the functions from previous tasks
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-if __name__ == "__main__":
-    try:
-        items = load_from_json_file(filename)
-    except FileNotFoundError:
-        items = []
+if os.path.exists(filename):
+    my_list = load_from_json_file(filename)
+else:
+    my_list = []
 
-    items.extend(sys.argv[1:])
+my_list.extend(sys.argv[1:])
 
-    save_to_json_file(items, filename)
+save_to_json_file(my_list, filename)
