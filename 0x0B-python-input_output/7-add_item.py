5ff3d6ea25a5c42a8a9559a68ba47a8aaf144ca9
medayoubadri <medayoubadri@gmail.com>
2024-10-15 18:51:59 +0100

    Task 6: Load a Python object from a JSON file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,9 +4,8 @@ Adds all command-line arguments to a Python list and saves them
 to a JSON file. It's like collecting things and hoarding them forever.
 """
 
-import sys
+from sys import argv
 
-# Dynamically import functions from previous tasks
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -17,6 +16,7 @@ try:
 except FileNotFoundError:
     items = []
 
-items.append(sys.argv[1:])
+for arg in argv[1:]:
+    items.append(arg)
 
 save_to_json_file(items, filename)
