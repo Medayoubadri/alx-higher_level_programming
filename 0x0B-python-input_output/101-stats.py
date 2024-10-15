9901154d8b8287fa66d2bfd1ef9f0830ec498153
medayoubadri <medayoubadri@gmail.com>
2024-10-15 19:38:14 +0100

    Task 101: Script that computes metrics from stdin and prints every 10 lines

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,7 +2,7 @@
 """
 Script that reads stdin line by line and computes metrics:
 - Input format: <IP Address> - [<date>]
-  "GET /projects/260 HTTP/1.1" <status code> <file size>
+"GET /projects/260 HTTP/1.1" <status code> <file size>
 - Prints statistics every 10 lines and on keyboard interruption (CTRL + C).
 """
 
@@ -11,7 +11,7 @@ import sys
 
 def print_stats(total_size, status_counts):
     """
-    Prints the total file size and the count ofstatus codes ascending order.
+    Prints the total file size and the count of status codes ascending order.
     """
     print(f"File size: {total_size}")
     for code in sorted(status_counts.keys()):
@@ -20,15 +20,8 @@ def print_stats(total_size, status_counts):
 
 
 total_size = 0
-status_counts = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0}
+status_counts = {200: 0, 301: 0, 400: 0, 401: 0,
+                 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 
 try:
@@ -36,16 +29,17 @@ try:
         line_count += 1
         parts = line.split()
 
-        try:
-            status_code = int(parts[-2])
-            file_size = int(parts[-1])
+        if len(parts) >= 2:
+            try:
+                status_code = int(parts[-2])
+                file_size = int(parts[-1])
 
-            total_size += file_size
+                total_size += file_size
 
-            if status_code in status_counts:
-                status_counts[status_code] += 1
-        except (IndexError, ValueError):
-            continue
+                if status_code in status_counts:
+                    status_counts[status_code] += 1
+            except (ValueError, IndexError):
+                pass
 
         if line_count % 10 == 0:
             print_stats(total_size, status_counts)
