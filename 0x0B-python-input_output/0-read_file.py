b0c004cb0f0189f7d48f2865056f5a150ec85e3d
medayoubadri <medayoubadri@gmail.com>
2024-10-15 17:18:56 +0100

    Task 0: Read and print a file (UTF8) and readme

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,8 +6,8 @@ just screaming at the screen.
 
 
 def read_file(filename=""):
-	"""
-	Reads a UTF8 text file and prints it to stdout.
-	"""
-	with open(filename, 'r', encoding='utf-8') as file:
-		print(file.read(), end="")
+    """
+    Reads a UTF8 text file and prints it to stdout.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read(), end="")
