8530c5f510321463dc3ad47f06319ea48886f4a2
medayoubadri <medayoubadri@gmail.com>
2024-10-15 19:09:37 +0100

    Task 10: Add filtering functionality to to_json method in Student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Defines a Student class with attributes first_name, last_name, and age.
+Includes a method to retrieve the dictionary representation of the instance
+with an optional filter for specific attributes.
+"""
+
+
+class Student:
+    """
+    Defines a student by their first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Instantiates a Student with first name, last name, and age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of the Student instance.
+        """
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            # Return only the attributes specified in the list
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        # Return all attributes
+        return self.__dict__
