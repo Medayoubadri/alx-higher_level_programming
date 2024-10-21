cf6b817e43fa93b74435d1b94761c6513ad03e32
medayoubadri <medayoubadri@gmail.com>
2024-10-21 11:50:50 +0100

    4. Area first

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,3 +83,12 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """
+        Calculate the area of the rectangle.
+
+        Returns:
+            int: The area value of the rectangle.
+        """
+        return self.width * self.height
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -29,6 +29,32 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r2.id, 2)
         self.assertEqual(r3.id, 12)
 
+    def test_attribute_validation(self):
+        """
+        Test the validation of attribute values.
+        """
+        with self.assertRaises(TypeError):
+            Rectangle(10, "2")
+        with self.assertRaises(ValueError):
+            r = Rectangle(10, 2)
+            r.width = -10
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, 2)
+            r.x = {}
+        with self.assertRaises(ValueError):
+            Rectangle(10, 2, 3, -1)
+
+    def test_area(self):
+        """
+        Test the area calculation of the Rectangle instance.
+        """
+        r1 = Rectangle(3, 2)
+        r2 = Rectangle(2, 10)
+        r3 = Rectangle(8, 7)
+        self.assertEqual(r1.area(), 6)
+        self.assertEqual(r2.area(), 20)
+        self.assertEqual(r3.area(), 56)
+
 
 if __name__ == "__main__":
     unittest.main()
