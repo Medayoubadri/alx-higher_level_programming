3de5431d44e6b198ce822c9954bdc09c90501730
medayoubadri <medayoubadri@gmail.com>
2024-11-19 15:31:56 +0100

    Task 17: Convert hbtn_0c_0 database, table, and field to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,5 @@
 -- Converts the entire database and table first_table to UTF8.
 
--- Convert the database default to utf8mb4
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert first_table to utf8mb4
-ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `hbtn_0c_0`
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
