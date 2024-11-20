87f932104c90d7317993b5864ecea36ec9e232bb
medayoubadri <medayoubadri@gmail.com>
2024-11-20 20:44:21 +0100

    Task 0: List MySQL user privileges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Task 0: My privileges!
+-- This script lists all privileges for 'user_0d_1' and 'user_0d_2' in localhost.
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
