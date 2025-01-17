6adfc9f813668f078e47a8a12a88080604e71a68
medayoubadri <medayoubadri@gmail.com>
2025-01-17 20:58:46 +0100

    Task 8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+    
+    payload = {'q': q}
+    url = 'http://0.0.0.0:5000/search_user'
+    r = requests.post(url, data=payload)
+    
+    try:
+        json_response = r.json()
+        if json_response:
+            print(f"[{json_response.get('id')}] {json_response.get('name')}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
