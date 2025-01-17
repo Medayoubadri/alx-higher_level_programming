0a81580c8e2858a60dbe3b8f5c0857d11c3d76e9
medayoubadri <medayoubadri@gmail.com>
2025-01-17 20:59:22 +0100

    Task 9. My GitHub!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=(username, password))
+    
+    try:
+        json_response = r.json()
+        print(json_response.get('id'))
+    except ValueError:
+        print("None")
