81b5ed3650fbda2e92f77a58d128a0a15236c0e7
medayoubadri <medayoubadri@gmail.com>
2025-02-11 23:28:52 +0100

    semistandards... fix-4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-
-if (process.argv.length < 3) {
-  console.error('Usage: ./4-starwars_count.js <API_URL>');
-  process.exit(1);
-}
-
-const apiUrl = process.argv[2];
-
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const films = JSON.parse(body).results;
-    const count = films.reduce((acc, film) => {
-      return acc + (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/') ? 1 : 0);
-    }, 0);
-    console.log(count);
-  }
+request(process.argv[2], (error, response, body) => {
+  console.log(
+    error ||
+    JSON.parse(body).results.filter((movie) =>
+      movie.characters
+        .includes('https://swapi-api.alx-tools.com/api/people/18/')
+    ).length
+  );
 });
