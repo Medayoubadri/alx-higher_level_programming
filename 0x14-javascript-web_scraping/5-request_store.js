d1bd7978127db5e2d1a8010294c0065820a98112
medayoubadri <medayoubadri@gmail.com>
2025-02-11 08:52:34 +0100

    Task 5. Loripsum a script that gets the content of a webpage

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const fs = require("fs")
+const request = require("request")
+
+if (process.argv.length < 4) {
+    console.error("Usage: ./5-request_store.js <URL> <file_path>")
+    process.exit(1)
+}
+
+const url = process.argv[2]
+const filePath = process.argv[3]
+
+request.get(url, (error, response, body) => {
+    if (error) {
+        console.error(error)
+    } else {
+        fs.writeFile(filePath, body, "utf-8", (err) => {
+            if (err) {
+                console.error(err)
+            }
+        })
+    }
+})
