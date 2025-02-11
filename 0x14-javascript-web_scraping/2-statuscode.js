f8d2380b6b5d40a08f72283b9326ec41a14cb887
medayoubadri <medayoubadri@gmail.com>
2025-02-11 22:53:11 +0100

    semistandards... fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,17 +1,3 @@
 #!/usr/bin/node
 const request = require('request');
-
-if (process.argv.length < 3) {
-  console.error('Usage: ./2-statuscode.js <URL>');
-  process.exit(1);
-}
-
-const url = process.argv[2];
-
-request.get(url, (error, response) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(`code: ${response.statusCode}`);
-  }
-});
+request(process.argv[2], (error, response) => console.log(`code: ${response && response.statusCode}`));
