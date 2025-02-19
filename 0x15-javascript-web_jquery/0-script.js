f8c41cf92e2e26fcd831f9a3a643b3cc25d87d14
medayoubadri <medayoubadri@gmail.com>
2025-02-19 23:01:44 +0100

    0x15. JavaScript - Web jQuery

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,5 +1,3 @@
-// Wait for the DOM to be fully loaded
 document.addEventListener('DOMContentLoaded', () => {
-  // Select the header element and change its color to red
   document.querySelector('header').style.color = '#FF0000';
 });
